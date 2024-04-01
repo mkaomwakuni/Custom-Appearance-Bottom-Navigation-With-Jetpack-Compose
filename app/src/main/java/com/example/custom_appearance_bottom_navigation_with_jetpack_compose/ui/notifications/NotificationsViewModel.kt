0effbbe48cc39b817516abad8f7591b051fd0ccc
mkaocodes <mkaoibrahim@gmail.com>
2024-04-01 11:23:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/custom_appearance_bottom_navigation_with_jetpack_compose/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.custom_appearance_bottom_navigation_with_jetpack_compose.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
